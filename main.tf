082c87298285675a1dcf17c4b18db415ba9a55e9
Mahdi <mahdi.khalil@siemens.com>
2023-04-21 02:23:52 +0800

    test

--- main.tf
+++ main.tf
@@ -1,9 +1,28 @@
-resource "aws_subnet" "main" {
+resource "aws_subnet" "SubnetDevops" {
   vpc_id     = data.aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
     Name = "Main"
+  }
+}
+
+resource "aws_security_group" "SGDevops" {
+  name_prefix = "DevopsCandidate-sg"
+  vpc_id = data.aws_vpc.vpc.id
+ 
+  ingress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["10.0.1.0/24"]
+  }
+ 
+  egress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -14,11 +33,15 @@ data "archive_file" "python_lambda_package" {
 }
 
 resource "aws_lambda_function" "lambda_function" {
-        function_name = "DevOps-Candidate-Lambda"
-        filename      = "API.zip"
-        source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
-        role          = data.aws_iam_role.lambda.arn
-        runtime       = "python3.6"
-        handler       = "lambda_function.lambda_handler"
-        timeout       = 10
+  function_name = "DevOps-Candidate-Lambda"
+  filename      = "API.zip"
+  source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
+  role          = data.aws_iam_role.lambda.arn
+  runtime       = "python3.6"
+  handler       = "lambda_function.lambda_handler"
+  timeout       = 10
+  vpc_config {
+    subnet_ids = [aws_subnet.SubnetDevops.id]
+    security_group_ids = [aws_security_group.SGDevops.id]
+  }
 }
